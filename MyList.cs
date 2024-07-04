c7db6a7b14e69849f0b38207bf57b01d7a90a9bb
Florence Murkfulsky <162458889+franklin13512@users.noreply.github.com>
2024-07-04 17:51:38 +0800

    Add files via upload

---
+++ MyList.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS1
+{
+    class MyList<T>
+    {
+        private T[] Data = new T[0];
+        private int Number = 0;
+
+        public int Capacity
+        {
+            get { return Data.Length; }
+        }
+        public int Count 
+        { 
+            get { return Number;} 
+        }
+
+        public void Add(T value)
+        {
+            if (Data.Length == 0) 
+            {
+                Data = new T[4];
+            }
+                
+            if (Number == Data.Length)
+            {
+                T[] Amid = new T[Data.Length * 2];
+                for (int i = 0; i < Data.Length; i++)
+                {
+                    Amid[i] = Data[i];
+
+                }
+                Data = Amid;
+            }
+            Data[Number] = value;
+            Number++;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if((index < 0) || (index > Count - 1))
+                {
+                    throw new ArgumentOutOfRangeException("序列号超出范围");
+                }
+
+                return Data[index];
+            }
+            set
+            {
+                Data[index] = value;
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS1
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            MyList<int> list = new MyList<int>();
+            list.Add(1);
+            list.Add(22);
+            list.Add(333);
+            list.Add(4444);
+            list.Add(55555);
+            list.Add(666666);
+            Console.WriteLine(list.Capacity);
+            Console.WriteLine(list.Count);
+
+            for(int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
